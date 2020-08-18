c31555da524e1589e4b59a89f9879981662decb5
Amina <amina.antoniazzi@gmail.com>
2020-08-18 15:11:49 +0100

    feat: draft homepage favourites carousel

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,12 +1,35 @@
 import React from "react";
 import Logout from "../components/Logout";
+import { Box, Button, Heading, Image } from "grommet";
+import bedroomImg from "../assets/bedroom-hero.jpeg";
+import AppBar from "../components/AppBar";
+import FavouritesCarousel from '../components/FavouritesCarousel'
+import { Notification } from "grommet-icons";
+
+const styles = {
+  color: "black",
+};
 
 function Home() {
   return (
-    <div>
-      Home page
-      <Logout />
+
+      <div >
+        <AppBar>
+          Hello There!
+          <Heading level="3" margin="none">
+            Login Page
+          </Heading>
+          <Button icon={<Notification />} onClick={() => {}} />
+
+        </AppBar>
+
+      <Box justify="center" align="center" background="white">
+        <Image fit="contain" fill src={bedroomImg} />
+        <Heading style={styles}>Welcome to announcement section</Heading>
+      </Box>
+      <FavouritesCarousel/>
     </div>
+    
   );
 }
 
