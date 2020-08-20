7af3c5713252442ef81a2cacd8f2cb046b4c4edf
neesafarza <aneesazafri@gmail.com>
2020-08-20 19:59:19 +0100

    refactor footer

--- client/src/views/Footer.tsx
+++ client/src/views/Footer.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Box, Grommet, Footer, Grid } from "grommet";
 import {
   Instagram,
   Twitter,
@@ -10,43 +11,43 @@ import "./Footer.scss";
 
 const Foot = () => {
   return (
-    <footer>
-      <div className="foot-info">
+    <Footer direction="column" pad={{"horizontal": "none", "vertical": "medium"}}>
+      <Box className="foot-info" direction="row" gap="xlarge">
+        <Grid className="about" alignSelf="start">
+          <h3>About Us</h3>
+          <p>
+            Here at Furniss, we are passionate about sustainable furniture. We
+            wanted to create a space where people can browse upcycled and
+            preloved furniture or sell their own.
+          </p>
+        </Grid>
 
-      <div className="about">
-        <h3>About Us</h3>
-        <p>
-          Here at Furniss, we are passionate about sustainable furniture. We
-          wanted to create a space where people can browse upcycled and preloved
-          furniture or sell their own.
-        </p>
-      </div>
+        <Grid className="help-and-advice">
+          <h3>Help & Advice</h3>
+          <p>Contact us</p>
+          <p>Frequently asked questions</p>
+          <p>COVID-19</p>
+          <p>Payment Options</p>
+          <p>Buying and care guides</p>
+          <p>Careers</p>
+        </Grid>
 
-      <div className="help-and-advice">
-        <h3>Help & Advice</h3>
-        <p>Contact us</p>
-        <p>Frequently asked questions</p>
-        <p>COVID-19</p>
-        <p>Payment Options</p>
-        <p>Buying and care guides</p>
-        <p>Careers</p>
-      </div>
+        <Grid className="find-us">
+          <h3>Find Us</h3>
+          <Box direction="row">
+            <Instagram color="offwhite" className="icon" />
+            <Twitter color="offwhite" className="icon" />
+            <Facebook color="offwhite" className="icon" />
+            <Pinterest color="offwhite" className="icon" />
+            <Youtube color="offwhite" className="icon" />
+          </Box>
+        </Grid>
+      </Box>
 
-      <div className="find-us">
-        <h3>Find Us</h3>
-        <div>
-          <Instagram color="offwhite" className="icon" />
-          <Twitter color="offwhite" className="icon" />
-          <Facebook color="offwhite" className="icon" />
-          <Pinterest color="offwhite" className="icon" />
-          <Youtube color="offwhite" className="icon" />
-        </div>
-      </div>
-      </div>
-      <div className="license">
+      <Box className="license">
         <p>Copyright &copy; 2020 All Rights Reserved by Furniss</p>
-      </div>
-    </footer>
+      </Box>
+    </Footer>
   );
 };
 
