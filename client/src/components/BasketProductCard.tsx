ede1205bf97535dae189e7b241820df3fa71ab79
Amina <amina.antoniazzi@gmail.com>
2020-08-25 22:14:29 +0100

    style: style basket screen

--- client/src/components/BasketProductCard.tsx
+++ client/src/components/BasketProductCard.tsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
-import { Box, Button, Image, Text } from "grommet";
+import {
+  Box,
+  Button,
+  Image,
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHeader,
+  TableRow,
+  Text,
+} from "grommet";
 import { Edit, Trash } from "grommet-icons";
 import { Product } from "../models/product";
 import { useHistory } from "react-router-dom";
 import ApiService from "../ApiService/ApiService";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "../styles/BasketProductCard.scss";
 
 interface Props {
   product: Product;
@@ -13,57 +25,68 @@ interface Props {
   basketProducts: any;
   setBasketProducts: (p: any) => void;
 }
-function BasketProductCard({ product, readonly, setBasketProducts, basketProducts }: Props) {
-
+function BasketProductCard({
+  product,
+  setBasketProducts,
+  basketProducts,
+}: Props) {
   // const [editmode, setEditMode] = useState(false);
   let history = useHistory();
 
   const handleDelete = () => {
-
-    ApiService.deleteProductFromBasket(product).then(() => {
-      toast(
-        <Box margin="20px">
-          {product.title} has been removed from your basket
-        </Box>
-      );
-    })
-    .then(() => {
-      let keepItems = basketProducts.filter((p: Product) => p.id !== product.id)
-      setBasketProducts(keepItems)
-    });
+    ApiService.deleteProductFromBasket(product)
+      .then(() => {
+        toast(
+          <Box margin="20px">
+            {product.title} has been removed from your basket
+          </Box>
+        );
+      })
+      .then(() => {
+        let keepItems = basketProducts.filter(
+          (p: Product) => p.id !== product.id
+        );
+        setBasketProducts(keepItems);
+      });
   };
   return (
-    <Box
-      height="medium"
-      width="small"
-      elevation="medium"
-      margin="medium"
-      pad="medium"
-      hoverIndicator="true"
-      round="small"
-    >
-      <Box
-        height="small"
-        width="small"
-        onClick={() => {
-          history.push({
-            pathname: "/productdetails",
-            search: `?id=${product.id}`,
-          });
-        }}
-      >
-        <Image fit="cover" src={`${product.images}`} />
-      </Box>
-      <Box direction="column" gap="small">
-        <Text>{product.title}</Text>
-        <Text size="small">{product.basket_quantity}</Text>
-        <Text size="small">{product.price} €</Text>
-      </Box>
-
-      <Box direction="row" gap="medium">
-        <Button icon={<Trash />} onClick={handleDelete} />
-      </Box>
-    </Box>
+    <TableBody>
+      <TableRow>
+        <TableCell scope="row" border="bottom">
+          <Box
+            margin="medium"
+            height="xsmall"
+            width="xsmall"
+            onClick={() => {
+              history.push({
+                pathname: "/productdetails",
+                search: `?id=${product.id}`,
+              });
+            }}
+          >
+            <Image fit="cover" src={`${product.images}`} />
+          </Box>
+        </TableCell>
+        <TableCell border="bottom" size="medium">
+          <Box direction="column" gap="small">
+            <Text size="large" color="headings" className="table-product-title">
+              {product.title}
+            </Text>
+          </Box>
+        </TableCell>
+        <TableCell scope="row" border="bottom">
+          <Text size="small">{product.basket_quantity}</Text>
+        </TableCell>
+        <TableCell border="bottom">
+          <Text size="small">{product.price} €</Text>
+        </TableCell>
+        <TableCell scope="row" border="bottom">
+          <Box direction="row" gap="medium">
+            <Button icon={<Trash />} onClick={handleDelete} />
+          </Box>
+        </TableCell>
+      </TableRow>
+    </TableBody>
   );
 }
 
--- client/src/views/Basket.tsx
+++ client/src/views/Basket.tsx
@@ -6,7 +6,18 @@ import "../styles/Basket.scss";
 import ApiService from "../ApiService/ApiService";
 import BasketProductCard from "../components/BasketProductCard";
 import { Product } from "../models/product";
-import { Box, Text } from "grommet";
+import {
+  Box,
+  Tabs,
+  Tab,
+  Heading,
+  Paragraph,
+  Table,
+  TableCell,
+  TableHeader,
+  TableRow,
+  Text,
+} from "grommet";
 import { useHistory } from "react-router-dom";
 import PaymentForm from "../components/Payment/PaymentForm";
 
@@ -36,8 +47,7 @@ function Basket({ isAuthenticated }: Props): JSX.Element {
   };
 
   useEffect(() => {
-    ApiService.getBasketProducts()
-      .then((res) => setBasketProducts(res))
+    ApiService.getBasketProducts().then((res) => setBasketProducts(res));
   }, []);
 
   useEffect(() => {
@@ -47,27 +57,83 @@ function Basket({ isAuthenticated }: Props): JSX.Element {
   }, [basketProducts]);
 
   return (
-    <div>
+    <Box>
       <CategoriesBar />
-      <h1>Your basket</h1>
-      <div className="basket-dashbaord">
+      <Heading
+        level="2"
+        color="text"
+        alignSelf="center"
+        margin={{ top: "large" }}
+      >
+        Your basket
+      </Heading>
+      <Box direction="column" className="basket-dashbaord">
         {basketProducts && basketProducts.length > 0 ? (
-          <Box margin="xlarge" pad="medium" align="center">
-            <div className="basket-container">
-              {renderProducts(basketProducts)}
-            </div>
-            <Text margin="large"> · · · </Text>
-            <Text>Almost There!</Text>
+          <Box width="100%" align="center">
+            <Box className="basket-container" direction="row" gap="medium">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableCell scope="col" border="bottom">
+                      <Text className="basket-table-heading">Item</Text>
+                    </TableCell>
+                    <TableCell scope="col" border="bottom"></TableCell>
+                    <TableCell scope="col" border="bottom">
+                      <Text className="basket-table-heading">Qty</Text>
+                    </TableCell>
+                    <TableCell scope="col" border="bottom">
+                      <Text className="basket-table-heading"> Unit price</Text>
+                    </TableCell>
+                    <TableCell scope="col" border="bottom"></TableCell>
+                  </TableRow>
+                </TableHeader>
+                {renderProducts(basketProducts)}
+              </Table>
+              {/* <Box
+                margin="medium"
+                justify="center"
+                height="100%"
+                width="medium"
+                background="offwhite"
+                align="center"
+                pad="small"
+              >
+                <p color="headings" className="basket-total-number">
+                  {amoutToPay}€
+                </p>
+                <Paragraph color="grey" className="basket-total-text">
+                  TOTAL
+                </Paragraph>
+                <a className="basket-total-btn">CHECKOUT NOW</a>
+              </Box> */}
+            </Box>
+
+            <Text margin="small"> · · · </Text>
+            <Heading
+              level="3"
+              color="text"
+              alignSelf="center"
+              margin={{ top: "medium" }}
+            >
+              Almost there
+            </Heading>
             <PaymentForm
               amoutToPay={amoutToPay}
               basketProducts={basketProducts}
             />
           </Box>
         ) : (
-            "No products on your basket"
-          )}
-      </div>
-    </div>
+          <Heading
+            level="3"
+            color="text"
+            alignSelf="center"
+            margin={{ top: "medium" }}
+          >
+            You don't have any products in your basket yet
+          </Heading>
+        )}
+      </Box>
+    </Box>
   );
 }
 
